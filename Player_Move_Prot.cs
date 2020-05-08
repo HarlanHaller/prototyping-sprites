85bf786be7b5f28c593feb1b5254b709740f55d3
Mr-Waffles <64970252+Mr-Waffles@users.noreply.github.com>
2020-05-08 11:06:45 -0400

    Add files via upload

---
+++ Player_Move_Prot.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move_Prot : MonoBehaviour
+{
+    public int playerSpeed = 10;
+    public bool facingRight = true;
+    public int playerJumpPower = 1250;
+    public float moveX;
+    public Rigidbody2D rd;
+
+    void Start() {
+        rd = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerMove();
+    }
+
+    void playerMove()
+    {
+        //controls
+        moveX = Input.GetAxis("Horizontal");
+        //TODO animation
+        //player direction
+        if (moveX < 0.0f && facingRight == false)
+        {
+            flipPlayer();
+        }
+        if (moveX <0.0f && facingRight == true)
+        {
+            flipPlayer();
+        }
+        //phisics
+        rd.velocity = new Vector2 (moveX * playerSpeed, rd.velocity.y);
+    }
+
+    void jump()
+    {
+        //TODO jumping code
+    }
+
+    void flipPlayer() 
+    {
+
+    }
+}
